80158fae7b28e61530e6bfd365dbe6c8d9fa8f5a
Nha Le (QA) <hoanha020294@gmail.com>
2023-08-25 14:49:42 +0700

    update topic 02

--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -16,7 +17,6 @@ public class Topic_02_Selenium_Locator {
 
     @BeforeClass
     public void beforeClass() {
-        // test jkjkhjk
         if (osName.contains("Windows")) {
             System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
         } else {
@@ -26,28 +26,66 @@ public class Topic_02_Selenium_Locator {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("https://www.facebook.com/");
+        driver.get("https://demo.nopcommerce.com/register");
         driver.manage().window().maximize();
     }
 
     @Test
-    public void TC_01_Url() {
+    public void TC_01_ID() {
+        driver.findElement(By.id("FirstName")).sendKeys("Nha Le");
+
+    }
+
+    @Test
+    public void TC_02_Class() {
+        driver.findElement(By.className("header-logo"));
+
+    }
+
+    @Test
+    public void TC_03_Name() {
+        driver.findElement(By.name("DateOfBirthDay"));
+
+    }
+    @Test
+    public void TC_04_TagName() {
+        driver.findElements(By.tagName("input"));
+
+    }
+
+    @Test
+    public void TC_05_LinkText() {
+        // lấy hết toàn bộ linktext đó, độ chính xác cao
+        driver.findElements(By.linkText("Shipping & returns"));
 
 
     }
 
     @Test
-    public void TC_02_Logo() {
+    public void TC_06_Partial_LinkText() {
+        //chỉ cần lấy 1 đoạn(đầu-giữa-cuối), độ chnh xác k cao
+        driver.findElements(By.partialLinkText("Apply for"));
+        driver.findElements(By.partialLinkText("Apply for vendor"));
+        driver.findElements(By.partialLinkText("vendor account"));
 
     }
 
     @Test
-    public void TC_03_Form() {
+    public void TC_07_XPath() {
+        driver.findElements(By.xpath("//*[@id=\"FirstName\"]"));
+
+    }
+
+    @Test
+    public void TC_08_Css() {
+        driver.findElements(By.cssSelector("input[id='FirstName']"));
+
 
     }
 
     @AfterClass
     public void afterClass() {
-        driver.quit();
+
+        //driver.quit();
     }
 }
