b41e53d0ae4a4d9952773b2c40fa2b63c700d454
Nha Le (QA) <hoanha020294@gmail.com>
2023-10-20 17:20:29 +0700

    update topic 07

--- selenium/webdriver/Topic_07_Textbox_Textarea.java
+++ selenium/webdriver/Topic_07_Textbox_Textarea.java
@@ -1,7 +1,9 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -33,23 +35,106 @@ public class Topic_07_Textbox_Textarea {
     @Test
     public void Login_01_Empty_Email_And_Password() {
         driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepSeconds(3);
+        driver.findElement(By.cssSelector("button#send2")).click();
+        //driver.findElement(By.xpath("//button[@class='button']")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-required-entry-email")).getText(), "This is a required field.");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-required-entry-pass")).getText(), "This is a required field.");
 
 
     }
 
     @Test
-    public void TC_02_Logo() {
+    public void Login_02_Invalid_Email() {
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepSeconds(3);
+
+        driver.findElement(By.cssSelector("input#email")).sendKeys("lelucy@1234");
+        driver.findElement(By.cssSelector("input#pass")).sendKeys("1234567890");
+        sleepSeconds(3);
+        driver.findElement(By.cssSelector("button#send2")).click();
+        sleepSeconds(3);
+        //driver.findElement(By.xpath("//button[@class='button']")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-validate-email-email")).getText(), "Please enter a valid email address. For example johndoe@domain.com.");
+
+
 
     }
 
     @Test
-    public void TC_03_Form() {
+    public void Login_03_Invalid_Password() {
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepSeconds(3);
+
+        driver.findElement(By.cssSelector("input#email")).sendKeys("lelucy90@gmail.com");
+        driver.findElement(By.cssSelector("input#pass")).sendKeys("12345");
+        sleepSeconds(3);
+        driver.findElement(By.cssSelector("button#send2")).click();
+        sleepSeconds(3);
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#advice-validate-password-pass")).getText(), "Please enter 6 or more characters without leading or trailing spaces.");
+
+    }
+
+    @Test
+    public void Login_04_Incorrect_Email_OR_Password() {
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepSeconds(3);
+
+        driver.findElement(By.cssSelector("input#email")).sendKeys("automationfc@gmail.com");
+        driver.findElement(By.cssSelector("input#pass")).sendKeys("123456");
+        sleepSeconds(3);
+        driver.findElement(By.cssSelector("button#send2")).click();
+        sleepSeconds(3);
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.error-msg span")).getText(), "Invalid login or password.");
+
+        driver.findElement(By.cssSelector("input#email")).sendKeys("automationfc@gmail.net");
+        driver.findElement(By.cssSelector("input#pass")).sendKeys("12345645756745");
+        sleepSeconds(3);
+        driver.findElement(By.cssSelector("button#send2")).click();
+        sleepSeconds(3);
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.error-msg span")).getText(), "Invalid login or password.");
+
+    }
+    @Test
+    public void Login_05_Success() {
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepSeconds(3);
+        // Register an account
+        driver.findElement(By.xpath("//a[@class='button']")).click();
+        driver.findElement(By.cssSelector("input#firstname")).sendKeys("Nha");
+        driver.findElement(By.cssSelector("input#lastname")).sendKeys("Le");
+        driver.findElement(By.cssSelector("input#email_address")).sendKeys("lelucy90@gmail.com");
+        driver.findElement(By.cssSelector("input#password")).sendKeys("123456");
+        driver.findElement(By.cssSelector("input#confirmation")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[@title='Register']")).click();
+
+
+//        driver.findElement(By.cssSelector("input#email")).sendKeys("automationfc@gmail.com");
+//        driver.findElement(By.cssSelector("input#pass")).sendKeys("123456");
+//        sleepSeconds(3);
+//        driver.findElement(By.cssSelector("button#send2")).click();
+
 
     }
 
     @AfterClass
     public void afterClass() {
         driver.quit();
+    }
+
+    public void sleepSeconds(long timeInsecond) {
+        try {
+            Thread.sleep(timeInsecond *1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
