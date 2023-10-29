d1cba29ce5bd13ee280db4138e2183a8013fb2e9
NhaLe94 <hoanha020294@gmail.com>
2023-10-29 21:44:42 +0700

    add topic 08

--- selenium/webdriver/Topic_08_Default_Dropdown.java
+++ selenium/webdriver/Topic_08_Default_Dropdown.java
@@ -4,15 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_08_Default_Dropdown {
 
     WebDriver driver;
+    String firstName = "Lucy", lastName = "Le", emailAddress = getEmailAddress(), password = "1234567890";
+    String companyName = "Testing";
+    String day, month, year;
     String projectPath = System.getProperty("user.dir");
     String osName = System.getProperty("os.name");
 
@@ -34,16 +39,26 @@ public class Topic_08_Default_Dropdown {
 
     @Test
     public void TC_01_Register() {
+
+
+
         driver.findElement(By.cssSelector("a.ico-register")).click();
-        driver.findElement(By.id("FirstName")).sendKeys("");
-        driver.findElement(By.id("LastName")).sendKeys("");
-        Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
+        driver.findElement(By.id("FirstName")).sendKeys(firstName);
+        driver.findElement(By.id("LastName")).sendKeys(lastName);
+
+       Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
+        day.selectByVisibleText("20");
+        // verfy dropdown này là single (k phải multiple)
+        Assert.assertFalse(day.isMultiple());
+        new Select(driver.findElement(By.name("DateOfBirthMonth"))).selectByVisibleText("April");
+        new Select(driver.findElement(By.name("DateOfBirthYear"))).selectByVisibleText("1994");
 
 
-        driver.findElement(By.id("Email")).sendKeys("");
-        driver.findElement(By.id("Company")).sendKeys("");
-        driver.findElement(By.id("Password")).sendKeys("");
-        driver.findElement(By.id("ConfirmPassword")).sendKeys("");
+        driver.findElement(By.id("Email")).sendKeys(emailAddress);
+        driver.findElement(By.id("Company")).sendKeys(companyName);
+        driver.findElement(By.id("Password")).sendKeys(password);
+        driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+        driver.findElement(By.cssSelector("button#register-button")).click();
 
     }
 
@@ -68,5 +83,10 @@ public class Topic_08_Default_Dropdown {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+    public String getEmailAddress(){
+        Random rand = new Random();
+        return "automation" + rand.nextInt(99999) + "@gmail.net";
+
     }
 }
