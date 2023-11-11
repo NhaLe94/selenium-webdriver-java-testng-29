14cb6d43b2fd501153580c4fb18b0fbfd40de1a6
NhaLe94 <hoanha020294@gmail.com>
2023-11-11 20:59:02 +0700

    update topic 11

--- selenium/webdriver/Topic_11_Button_Radio_Checkbox.java
+++ selenium/webdriver/Topic_11_Button_Radio_Checkbox.java
@@ -1,9 +1,11 @@
 package webdriver;
 
+import com.beust.ah.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -45,11 +47,32 @@ public class Topic_11_Button_Radio_Checkbox {
         // verify button enabled khi click vào checkbox
         Assert.assertTrue(registerButton.isEnabled());
 
+        // lay ra ma mau nen cua button
+
+        String registerBackgroundColorCode = registerButton.getCssValue("background-color");
+        System.out.println("Background Color = "+ registerBackgroundColorCode);
+        Color registerBackgroundColor = Color.fromString(registerBackgroundColorCode);
+        String registerBackgroundHexa = registerBackgroundColor.asHex();
+        System.out.println("Background color hexa = " + registerBackgroundHexa);
+        System.out.println("Background color hexa = " + registerBackgroundHexa.toUpperCase());
+        System.out.println("Background color hexa = " + registerBackgroundHexa.toLowerCase());
+        Assert.assertEquals(registerBackgroundHexa.toLowerCase(), "#ef5a00");
+
     }
 
     @Test
-    public void TC_02_Logo() {
+    public void TC_02_Fahasa_Button() {
+        driver.get("https://www.fahasa.com/customer/account/create");
+        driver.findElement(By.cssSelector("li.popup-login-tab-login")).click();
+        WebElement loginButton = driver.findElement(By.cssSelector("button.fhs-btn-login"));
+        Assert.assertFalse(loginButton.isEnabled());
+        System.out.println(loginButton.getCssValue("background-color"));
 
+        Assert.assertEquals(Color.fromString(loginButton.getCssValue("background-color")).asHex().toLowerCase(),"#000000");
+        driver.findElement(By.cssSelector("input#login_username")).sendKeys("lelucy90@gmail.com");
+        driver.findElement(By.cssSelector("input#login_password")).sendKeys("123456");
+        Assert.assertTrue(loginButton.isEnabled());
+        Assert.assertEquals(Color.fromString(loginButton.getCssValue("background-color")).asHex().toLowerCase(),"#c92127");
     }
 
     @Test
