946ddfd10a214f669fe06b1472ab063db7b78fbf
Nha Le (QA) <hoanha020294@gmail.com>
2023-11-30 17:21:26 +0700

    update topic 15

---
+++ selenium/webdriver/Topic_15_Popup_02.java
@@ -0,0 +1,58 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_15_Popup_02 {
+
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_Random_Not_In_DOM() {
+        driver.get("https://www.javacodegeeks.com/");
+
+
+
+    }
+
+    @Test
+    public void TC_02_Logo() {
+
+    }
+
+    @Test
+    public void TC_03_Form() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+
+}
