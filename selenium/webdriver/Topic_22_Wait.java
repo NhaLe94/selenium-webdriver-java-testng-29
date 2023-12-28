fe6050e9224fbed68e625745fd832fc7a335b2d0
Nha Le (QA) <hoanha020294@gmail.com>
2023-12-28 16:57:15 +0700

    update topic 22

--- selenium/webdriver/Topic_22_Wait.java
+++ selenium/webdriver/Topic_22_Wait.java
@@ -10,15 +10,13 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class Topic_00_Template {
-
+public class Topic_22_Wait {
     WebDriver driver;
     String projectPath = System.getProperty("user.dir");
     String osName = System.getProperty("os.name");
 
     @BeforeClass
     public void beforeClass() {
-
         if (osName.contains("Windows")) {
             System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
         } else {
@@ -28,30 +26,27 @@ public class Topic_00_Template {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("https://www.facebook.com/");
-        driver.manage().window().maximize();
     }
 
     @Test
     public void TC_01_Url() {
-
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
     }
 
     @Test
     public void TC_02_Logo() {
-
+        Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
     }
 
     @Test
     public void TC_03_Form() {
-
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
     }
 
     @AfterClass
     public void afterClass() {
         driver.quit();
     }
-
     public void sleepSeconds(long timeInsecond) {
         try {
             Thread.sleep(timeInsecond *1000);
