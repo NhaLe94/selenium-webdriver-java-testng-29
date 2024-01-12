7b8bb1c4acc295e67bb5ae9d489046afeb5fd3be
Nha Le (QA) <hoanha020294@gmail.com>
2024-01-12 17:24:51 +0700

    update topic 22

--- selenium/webdriver/Topic_22_Wait.java
+++ selenium/webdriver/Topic_22_Wait.java
@@ -3,15 +3,20 @@ package webdriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_22_Wait {
     WebDriver driver;
+    WebDriverWait explicitWait;
+    By reconfirmEmailTextBox = By.cssSelector("input[name='reg_email_confirmation__']");
     String projectPath = System.getProperty("user.dir");
     String osName = System.getProperty("os.name");
 
@@ -24,19 +29,51 @@ public class Topic_22_Wait {
         }
 
         driver = new FirefoxDriver();
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
     }
 
     @Test
-    public void TC_01_Url() {
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
+    public void TC_01_Visible() {
+        driver.findElement(By.cssSelector("a[data-testid='open-registration-form-button']")).click();
+        sleepSeconds(2);
+
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).sendKeys("nha@gmail.com");
+        sleepSeconds(2);
+
+        //tại đng thời điểm này/step này thì confirm email textbox đang visible/displayed
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(reconfirmEmailTextBox));
+
+        // kiểm tra 1 element đang hiển thị
+        Assert.assertTrue(driver.findElement(reconfirmEmailTextBox).isDisplayed());
+
     }
 
     @Test
-    public void TC_02_Logo() {
-        Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
+    public void TC_02_Invisible_In_DOM() {
+        // Điều kiện 2 - Element không xuất hện trên UI và vẫn có trong cây HTML
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).clear();
+        sleepSeconds(2);
+        //tại đng thời điểm này/step này thì confirm email textbox đang visible/displayed
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(reconfirmEmailTextBox));
+        // kiểm tra 1 element k hiển thị
+        Assert.assertFalse(driver.findElement(reconfirmEmailTextBox).isDisplayed());
+
     }
+    @Test
+    public void TC_03_Invisible_NOT_In_DOM() {
+        // Điều kiện 3 - Element không xuất hện trên UI và k có trong cây HTML
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).clear();
+        sleepSeconds(2);
+        //tại đng thời điểm này/step này thì confirm email textbox đang visible/displayed
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(reconfirmEmailTextBox));
+        // kiểm tra 1 element k hiển thị
+        Assert.assertFalse(driver.findElement(reconfirmEmailTextBox).isDisplayed());
+
+    }
+
 
     @Test
     public void TC_03_Form() {
